cb26bdf608acc0fbb204539d9dfa52a65bbaa86a
Miguel Quintal <a7347@esap.edu.pt>
2025-10-30 12:14:35 +0000

    ..

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ola");
+            Console.WriteLine("olafrgrg");
         }
     }
 }
